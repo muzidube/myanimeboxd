761c46169993eb9179560717f08800dffef14743
muzidube <muzidube1@gmail.com>
2021-12-15 16:01:47 +0000

    Finally got typescript to play nice with node/express

--- server/api/anime.js
+++ server/api/anime.js
@@ -3,24 +3,21 @@ require('dotenv').config();
 const axios = require('axios');
 
 const router = express1.Router();
-
-router.get('/anime/top-airing', (request: any, response: any) => {
-  const config: {} = {
+router.get('/movieAPI/popular', (request, response) => {
+  const config = {
     method: 'get',
     url: "https://api.myanimelist.net/v2/anime/ranking?ranking_type=airing&limit=10'",
     headers: {
       'X-MAL-CLIENT-ID': process.env.X_MAL_CLIENT_ID
     }
   };
-
   axios(config)
-    .then((popularResponse: any) => {
+    .then((popularResponse) => {
       console.log(JSON.stringify(popularResponse.data));
       response.json(JSON.stringify(popularResponse.data));
     })
-    .catch((error: Error) => {
+    .catch((error) => {
       console.log(error);
     });
 });
-
 module.exports = router;
--- server/server.ts
+++ server/server.ts
@@ -4,8 +4,11 @@ const anime = require('./api/anime.js');
 
 const app = express();
 const port = process.env.PORT || 5000;
+
 // app.use(cors({ origin: 'https://www.consoom.co.uk' }));
+
 app.use('/', anime);
+
 app.listen(port, () => {
   console.log(`Server running successfully at ${port}`);
 });
