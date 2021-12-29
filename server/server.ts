faad13157bf737d645409d33616dc501bc36414d
muzidube <muzidube1@gmail.com>
2021-12-29 19:22:33 +0000

    Changing cors

--- server/server.ts
+++ server/server.ts
@@ -6,7 +6,7 @@ const anime = require('./routes/routes.js');
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(cors({ origin: 'http://localhost:8080' }));
+app.use(cors());
 
 app.use('/anime', anime);
 
